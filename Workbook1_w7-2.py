225d7fedbce971658d76a6b9db52d8522a28488e
viviv31 <57225062+viviv31@users.noreply.github.com>
2020-04-02 20:53:23 -0400

    Add files via upload

---
+++ Workbook1_w7-2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+location = "downloads/DC.csv"
+df = pd.read_csv(location)
+df.head
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
